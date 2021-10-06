e4ee1ac38a0249b71f118fc5bfb3139dfa2a5216
SporoZoite <lisavanhooydonk@gmail.com>
2021-10-06 19:25:15 +0200

    added style to contact page without map plugin

--- wordpress/wp-content/themes/PIL/page-templates/page-contact.php
+++ wordpress/wp-content/themes/PIL/page-templates/page-contact.php
@@ -52,13 +52,18 @@ $container = get_theme_mod( 'understrap_container_type' );
       </div>
 
 	  <div class="contact-map">
-	  <?php the_field('map'); ?>
+	  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23906.18178591372!2d5.562535538794178!3d50.64000776927387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0f0abf7d8c7d3%3A0xcafe05be0243b139!2sLe%20P%C3%B4le%20Image%20de%20Li%C3%A8ge!5e0!3m2!1sfr!2snl!4v1633538861046!5m2!1sfr!2snl"
+		width="1324" height="624" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 	  <img id="contact-big-pointer" src="<?php echo get_template_directory_uri(); ?>/img/marker-map.svg" alt="pointer">
 	  </div>
     </div>
 
 </div>
 </div>
+</div>
+
+<div class="contact-horizontal-divider">
+<span class="contact-horizontal-line"></span>
 </div>
 
 
