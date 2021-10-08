e6c503a71d36cbd755f9ddadf11b3d734c086ba8
irisrenauld <iris.renauld7@gmail.com>
2021-10-08 15:43:18 +0200

    modif home-formation

--- wordpress/wp-content/themes/PIL/front-page.php
+++ wordpress/wp-content/themes/PIL/front-page.php
@@ -213,7 +213,7 @@ $picture1 = $image1['sizes']['large'];
 		</div>
 	</div>
 
-	<img scr="<?php echo get_template_directory_uri();?>/img/Mask Group 89@2x.jpg" id="home-formations-image">
+	<img scr="<?php echo get_template_directory_uri(); ?>/img/Mask Group 89@2x.jpeg" id="home-formations-image">
 
 	<h1 id="home-formations-titre">Formations audiovisuel</h1>
 	<p id="home-formations-subtitle">Vous souhaitez vous former à un métier ou une compétence en pénurie en Wallonie ?</p>
